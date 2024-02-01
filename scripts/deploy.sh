f9725ac7e51b3f937c17097e43b34ee0642d8ee9
chan_nie <126068623+chennielee@users.noreply.github.com>
2024-02-01 16:22:34 +0900

    mod deply

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,34 +1,21 @@
-REPOSITORY=/home/ubuntu/app
+REPOSITORY=/home/ubuntu/webian-back
 cd $REPOSITORY
 
-PROJECT_ROOT="/home/ubuntu/webian-back"
+APP_NAME=Uhang
 
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+JAR_NAME=/home/ec2-user/app/build/libs/backend-0.0.1-SNAPSHOT.jar
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f java)
 
-TIME_NOW=$(date +%c)
-
-# build 파일 복사
-echo "$TIME_NOW > $JAR_PATH 확인" >> $DEPLOY_LOG
-JAR_NAME=$(ls $PROJECT_ROOT/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$PROJECT_ROOT/build/libs/$JAR_NAME
-
-CURRENT_PID=$(pgrep -f $JAR_NAME)
-
-if [ -z $CURRENT_PID ]
+if [ -z $CURRENT_PID ] #2
 then
-  echo "> 종료할 애플리케이션이 없습니다."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
+  echo "> kill -15 $CURRENT_PID"
   sudo kill -15 $CURRENT_PID
   sleep 5
 fi
 
-# jar 파일 실행
-echo "$TIME_NOW > $JAR_NAME 파일 실행" >> $DEPLOY_LOG
-nohup java -jar -Duser.timezone=Asia/Seoul $JAR_PATH --logging.level.org.hibernate.SQL=DEBUG > $APP_LOG 2> $ERROR_LOG &
-
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+echo "> $JAR_PATH 배포" #3
+nohup java -jar $JAR_NAME > /home/ec2-user/nohup.out 2> /dev/null &
