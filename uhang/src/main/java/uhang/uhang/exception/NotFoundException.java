a3eb35231515e5610e07d79aee1fec96eaed02c7
VIIXXV <jiminiyooni@sookmyung.ac.kr>
2024-02-02 05:59:27 +0900

    Revert "Merge pull request #6 from Team-U-Hang/feature/sortpost"
    This reverts commit 0ed63128ed61c52bd9e3000b3bf75449104db42f, reversing
    changes made to b5ccf50bea5a3a86fb68371b3aa3f851d2a98436.

--- uhang/src/main/java/uhang/uhang/exception/NotFoundException.java
+++
@@ -1,4 +0,0 @@
-package uhang.uhang.exception;
-
-public class NotFoundException extends RuntimeException {
-}
--- uhang/src/main/java/uhang/uhang/login/domain/repository/MemberRepository.java
+++ uhang/src/main/java/uhang/uhang/login/domain/repository/MemberRepository.java
@@ -5,7 +5,6 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import uhang.uhang.commentlike.dto.ClikeDto;
 import uhang.uhang.login.domain.Member;
 
 import java.util.List;
@@ -15,7 +14,5 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     public Member findByMemberEmail(String memberEmail);
     public boolean existsByMemberEmail(String email);
-
-    public Member findByMemberId(int memberId);
 }
 
